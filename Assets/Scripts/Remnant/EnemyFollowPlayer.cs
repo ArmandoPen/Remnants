6cabc74a6479c7bb2c257e87e29ee5d59c6013e2
Jrocket120 <101006636+Jrocket120@users.noreply.github.com>
2022-08-09 14:42:53 -0400

    Revert "Revert "Merge branch 'main' of https://github.com/ArmandoPen/Remnants""
    This reverts commit 8f3a94c19f4baf4ea4162b067ede37914eff25d0.

--- Assets/Scripts/Remnant/EnemyFollowPlayer.cs
+++ Assets/Scripts/Remnant/EnemyFollowPlayer.cs
@@ -7,6 +7,8 @@ public class EnemyFollowPlayer : MonoBehaviour
     public float speed;
     public float lineOfSite;
     public float shootingRange;
+    public float fireRate = 1f;
+    private float nextFireTime;
     public GameObject bullet;
     public GameObject bulletParent;
     private Transform player;
@@ -24,9 +26,10 @@ public class EnemyFollowPlayer : MonoBehaviour
         {
             transform.position = Vector2.MoveTowards(this.transform.position, player.position, speed * Time.deltaTime);
         }
-        else if (distanceFromPlayer <= shootingRange)
+        else if (distanceFromPlayer <= shootingRange && nextFireTime < Time.time)
         {
             Instantiate(bullet, bulletParent.transform.position, Quaternion.identity);
+            nextFireTime = Time.time + fireRate;
         }
     }
     private void OnDrawGizmosSelected()
